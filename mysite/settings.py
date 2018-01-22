6b2357880336e1e1b81eacc569e465048159f1ee
amit <satpute.amitkumat.gmail.com>
2018-01-22 18:32:24 +0530

    'req3'

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 SECRET_KEY = '8qhpc0)2(i69_lsgido(uiit0k19!cf=&$yxf-h&(k_sl1m8i6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
